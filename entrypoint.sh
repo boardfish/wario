3840394c7dced1ce9ed0f03f23c6184b616fa877
Simon Fish <si@mon.fish>
2020-08-29 14:54:37 +0100

    Remove asset precompile task

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ set -x
 rm -f /usr/src/app/tmp/pids/server.pid
 
 bundle check || bundle install
-yarn install --check-files
-bundle exec rails assets:precompile
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
