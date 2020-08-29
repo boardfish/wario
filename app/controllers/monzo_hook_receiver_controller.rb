5c7ff657acd75c4bba29eba6d3d970c23e9c43c5
Simon Fish <si@mon.fish>
2020-08-29 16:20:39 +0100

    Set up webhook

---
+++ app/controllers/monzo_hook_receiver_controller.rb
@@ -0,0 +1,18 @@
+# Receiver for Monzo webhook
+class MonzoHookReceiverController < ApplicationController
+  def transaction
+    MonzoTransaction.create!(metadata: transaction_params)
+  end
+
+  private
+
+  def transaction_params
+    params.require(:data).permit(
+      :account_id, :amount, :created, :currency, :description, :id, :category, :is_load, :settled,
+      merchant: [
+        :created, :group_id, :id, :logo, :emoji, :name, :category,
+        address: %i[address city country latitude longitude postcode region]
+      ]
+    )
+  end
+end
---
+++ spec/requests/monzo_hook_receiver_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'MonzoHookReceivers', type: :request do
+  describe 'GET /transaction' do
+    context 'with correct data' do
+      subject(:request) { post '/monzo_hook_receiver/transaction', params: data, headers: { 'CONTENT_TYPE' => 'application/json' } }
+      let(:data) { file_fixture('monzo_webhook_data.json').read }
+
+      it 'returns http success' do
+        request
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'creates a new MonzoTransaction' do
+        expect { request }.to change(MonzoTransaction, :count).by(1)
+      end
+
+      it 'creates a MonzoTransaction with the right data' do
+        request
+        expect do
+          MonzoTransaction.where("metadata ->> 'id' = :id", id: JSON.parse(data).dig('data', 'id')).limit(1).take!
+        end.not_to raise_error
+      end
+    end
+  end
+end
